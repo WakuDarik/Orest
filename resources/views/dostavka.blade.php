04ff60e30547a36f7031c3fd1e9c8a1203aa9c00
Dariy <waku108@gmail.com>
2020-05-04 16:54:20 +0300

    deli fix

--- resources/views/dostavka.blade.php
+++ resources/views/dostavka.blade.php
@@ -9,42 +9,7 @@
             <div class="delivery-form">
                 <div class="contact-form">
                     {!! $html->firstWhere("loacte_code", 'dostavka')->content !!}
-                    {{-- <h3>Доставка от 100 грн</h3>
-                    <form class="ajax_form radio-time" method="POST" action="{{route('delivery')}}"> --}}
-                    <fieldset>
-                        <div class="item">
-                            <div class="check-list">
-                                <div class="check-item">
-                                    <label>
-                                        <input type="radio" name="time" checked="checked" value="На сегодня - 100 грн">
-                                        <div>На<br> сьогодні</div>
-                                    </label>
-                                </div>
-                                <div class="check-item">
-                                    <label>
-                                        <input type="radio" name="time" value="На завтра - 100 грн">
-                                        <div>На<br> завтра</div>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="item name">
-                            <label>
-                                <input type="text" name="name" required="required" value="">
-                                <span class="item_label">Фамилия и имя</span>
-                            </label>
-                        </div>
-                        <div class="item phone">
-                            <label>
-                                <input type="text" id="phone" placeholder="+38(0__) ___-__-__" name="phone"
-                                    required="required" value="">
-                                <span class="item_label">Мобильный телефон</span>
-                            </label>
-                        </div>
-                        <input type="submit" name="formContact" value="Отправить" class="btn btn-red">
-                    </fieldset>
-                    @csrf
-                    </form>
+
                 </div>
             </div>
         </div>
