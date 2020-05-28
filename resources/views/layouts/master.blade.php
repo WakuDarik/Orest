e9588a00bceea78d3c8b313ae43b6cffd45948e0
Dariy <waku108@gmail.com>
2020-05-28 13:48:58 +0300

    asdasd

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/touch-sideswipe.css">
-
+    {{@dump(session('orderId'))}}
     <style>
         * {
             box-sizing: border-box;
