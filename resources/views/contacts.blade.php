fb1cd4230616d6857052a0bf77023f47099b7d2b
Dariy <waku108@gmail.com>
2021-03-18 13:01:07 +0200

    contacts fix

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -8,14 +8,17 @@
         <div class="row">
             <h1 class="page-title" style="width:100%">{!! $html->firstWhere("loacte_code", 'contacts')->title !!}
             </h1>
+            <div class="info">
+                {!! $html->firstWhere("loacte_code", 'contacts')->content !!}
+            </div>
             <div class="contact-map">
-                <div class="info">
+                {{-- <div class="info">
                     {!! $html->firstWhere("loacte_code", 'contacts')->content !!}
-                </div>
-                <iframe
+                </div> --}}
+                {{-- <iframe
                     src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1803.4581455605448!2d23.943954830869224!3d49.78888859835206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1588430741026!5m2!1sru!2sua"
                     width="100%" height="600" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
-                    tabindex="0"></iframe>
+                    tabindex="0"></iframe> --}}
             </div>
 
         </div>
